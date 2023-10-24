703719cf5db8ea8f9e23a8c81dfb1198723356a0
jsh-ssit <lunaharicot@naver.com>
2023-10-24 15:10:24 +0900

    Java Resources 아래 src 아래 teammember.jeonsh.test 패키지 아래 TeamMemberJeonsh.java 클래스 입니다.

---
+++ SecondGitTest/src/main/java/teammember/jeonsh/test/TeamMemberJeonsh.java
@@ -0,0 +1,11 @@
+package teammember.jeonsh.test;
+
+public class TeamMemberJeonsh {
+
+	public static void main(String[] args) {
+
+		System.out.println("Java Resources 아래 src 아래 teammember.jeonsh.test 패키지 아래 TeamMemberJeonsh.java 클래스 입니다.");
+
+	}
+
+}
